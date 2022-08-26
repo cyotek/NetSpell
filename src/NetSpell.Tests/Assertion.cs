06e11b618a4293e109d6cd26a89475a0f85a8110
Richard Moss <richard.moss@cyotek.com>
2022-08-26 17:01:03 +0100

    REF: Updated solution and project files

---
+++ src/NetSpell.Tests/Assertion.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+namespace NetSpell.Tests
+{
+  internal static class Assertion
+  {
+    #region Public Methods
+
+    public static void AssertEquals<T>(string message, T expected, T actual)
+    {
+      Assert.AreEqual(expected, actual, message);
+    }
+
+    public static void Fail(string message)
+    {
+      Assert.Fail(message);
+    }
+
+    #endregion Public Methods
+  }
+}
